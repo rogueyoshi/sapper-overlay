e66ccb45de491fa4a45dcee62feee0d4a100f730
Al Murray <me@rogueyoshi.com>
2020-09-17 03:08:46 -0400

    Update index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,5 @@
+<h1>Demos</h1>
 <ul>
-  <li><a href='/overlay/dbfz/main'>/overlay/dbfz/main</a></li>
+  <li><a href='/overlay/dbfz/main'>DBFZ Main Overlay</a></li>
 </ul>
+<h2><a href='https://github.com/rogueyoshi/sapper-overlay'>Source</a></h2>
